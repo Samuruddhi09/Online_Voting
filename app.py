d50e9cba34bbc1a43c1ebb2be3bb713ca7cadcac
Samruddhi Vispute <samruddhikvispute@gmail.com>
2026-01-19 11:28:22 +0530

    Phase 10.2: Add home route and fix Not Found error

--- app.py
+++ app.py
@@ -19,6 +19,12 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+#home page
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
 # Register Page
 @app.route("/register", methods=["GET", "POST"])
 def register():
